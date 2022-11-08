df8be3d72e22c011820737e302b26f4a1b499a81
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2022-11-08 13:52:07 +0900

    pull upstream

--- TeamHw_3_Code/main.py
+++ TeamHw_3_Code/main.py
@@ -12,7 +12,7 @@ def result():
       result['Name'] = request.form.get('Name')
 
       # 학번
-      result['Gender'] = request.form.get('Gender')# 성별
+      result['Gender'] = request.form.get('Gender')# 성별을 get을 통해 flask에서 gender데이터를 받아옵니다.
       result['Major']=request.form.get('Major') # 학과
 
       # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
